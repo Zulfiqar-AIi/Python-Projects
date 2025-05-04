b112927d4c14b0ddbcc12e3104f8052fdf33c797
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-05-04 23:06:57 +0500

    Lab1-Done

---
+++ Lab1-DS.py
@@ -0,0 +1,15 @@
+# Assign values to variables
+a = 10
+b = 5
+
+# Perform arithmetic operations
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b  # This will give a float result
+
+# Display the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
